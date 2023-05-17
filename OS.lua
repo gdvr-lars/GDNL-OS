71b7bdde2ae36519fb32ebfe3a965943302410cd
DevNettrix <88558252+DevNettrix@users.noreply.github.com>
2023-05-18 00:07:28 +0200

    Updated OS.lua

--- OS.lua
+++ OS.lua
@@ -24,3 +24,23 @@ function dofile(path)
         error(reason)
     end
 end
+
+package = {
+	paths = {
+		["/Libraries/"] = true
+  },
+  loaded = {}
+  loading = {}
+}
+
+local function requireExists(path)
+  return bootFilesystemProxy.exists(path)
+end
+
+function require(module)
+  local lowerModule = unicode.lower(module)
+  
+  if package.loaded[lowerModule] them
+    return package.loaded[lowerModule]
+  elseif package.loading[lowerModule] then
+    error("recursive require() call found: library \"" .. module .. "\" is trying to require another library that requires it\n" .. debug.traceback())
